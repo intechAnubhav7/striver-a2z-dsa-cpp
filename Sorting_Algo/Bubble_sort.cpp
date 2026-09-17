ba72f7dc459b0adb18f0f996adf4384db9765d94
Anubhav <anubhavsinghna2006@gmail.com>
2026-09-17 05:39:48 +0530

    Add Bubble Sort implementation in Bubble_sort.cpp
    Implement Bubble Sort algorithm in C++.

---
+++ Sorting_Algo/Bubble_sort.cpp
@@ -0,0 +1,29 @@
+// Bubble Sort
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
+#include<bits/stdc++.h>
+using namespace std;
+void bubble_sort(int arr[] , int n){
+    int temp;
+    for(int i=n-1; i>=1; i--){
+        for(int j=0; j<=i-1; j++){
+            if(arr[j]>arr[j+1]){
+               temp = arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;;
+            }
+        }
+    }
+}
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++) arr[i];
+    bubble_sort(arr , n);
+    for(int i=0; i<n; i++){
+        cout<< arr[i] << " ";
+    }
+    return 0;
+}
